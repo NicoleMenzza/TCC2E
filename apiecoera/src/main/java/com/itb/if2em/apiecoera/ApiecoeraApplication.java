ffc22a480f1f9f56f310e7aefed617299242f691
NicoleMenzza <rm99622@estudante.fieb.edu.br>
2026-06-18 11:33:51 +0000

    Criado api spring boot

---
+++ apiecoera/src/main/java/com/itb/if2em/apiecoera/ApiecoeraApplication.java
@@ -0,0 +1,13 @@
+package com.itb.if2em.apiecoera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiecoeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiecoeraApplication.class, args);
+	}
+
+}
---
+++ apiecoera/src/test/java/com/itb/if2em/apiecoera/ApiecoeraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.if2em.apiecoera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiecoeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
